d21e7c41a4a13cdf5c5dbe2d267756a473a1cef9
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2020-05-21 19:17:12 +0300

    added new line to demo collaborating on a project

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
 // comment example
    public static void main(String[] args) {
 	System.out.println("Hello World! We are learning VCS");
+	System.out.println("new line");
    }
 }
